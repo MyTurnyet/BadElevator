9317c5f4fa27a97cc1c4fbbf5d78874b874e536b
Paige Watson <paige@industriallogic.com>
2023-08-30 08:34:41 -0700

    clean up joke class

--- src/main/java/Elevator/Bling/JokeForTheRide.java
+++ src/main/java/Elevator/Bling/JokeForTheRide.java
@@ -2,9 +2,6 @@ package Elevator.Bling;
 
 import Elevator.ElevatorLogger;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 import java.io.IOException;
 
@@ -17,23 +14,9 @@ public class JokeForTheRide
 
     public Joke getJoke(boolean p) throws IOException, InterruptedException
     {
-//        OkHttpClient httpClient = new OkHttpClient();
-
-        String url ="";
-        if(p==true){
-            url ="https://elevator-jokes.herokuapp.com/jokes/programming/random";
-        } else {
-            url ="https://elevator-jokes.herokuapp.com/jokes/random";
-        }
-
-//        Request request = new Request.Builder()
-//                .url(url)
-//                .build();
-//        Response response = httpClient.newCall(request).execute();
         ObjectMapper om = new ObjectMapper();
-        String jokeType = p?"programming":"general";
+        String jokeType = p ? "programming" : "general";
         final String jokeJson = "{\"id\":\"1\",\"type\":\"%s\",\"setup\":\"some setup line\",\"punchline\":\"something funny!\"}".formatted(jokeType);
-//        Joke joke = om.readValue(response.body().string().toString(), Joke.class);
         Joke joke = om.readValue(jokeJson, Joke.class);
         ElevatorLogger.Info(joke.setup);
         ElevatorLogger.Info(joke.punchline);
